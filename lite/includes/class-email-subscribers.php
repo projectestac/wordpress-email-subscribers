1580a008ab4686e3ed3d80337eed149ab1e2aa82
Alejandro Escobar <alejandroescobar.98@gmail.com>
2024-06-26 11:39:35 +0200

    Fixed confirmation email not being sent

--- lite/includes/class-email-subscribers.php
+++ lite/includes/class-email-subscribers.php
@@ -1413,16 +1413,7 @@ if ( ! class_exists( 'Email_Subscribers' ) ) {
 
 				// Start-IG-Code.
 
-				// 2021.04.30 @aginard
-                if (is_xtec_super_admin()) {
-				// ************ Fi
-
 				require_once plugin_dir_path( __FILE__ ) . 'libraries/action-scheduler/action-scheduler.php';
-
-				// XTEC ************ AFEGIT - Blocked access to action scheduler to all users but xtecadmin
-                // 2021.04.30 @aginard
-				}
-				// ************ Fi
 
 				// End-IG-Code.
 
